3b3ae6bbabe943821e89beddbd4c4c4620da7a7f
neerajmaurya07 <neerajbittern104@gmail.com>
2016-09-05 13:06:31 +0530

    fist time initial project in git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mkhardware::Application.config.secret_key_base = 'f1326a1d3cd7a22a9d3b58ac5ef33b3d2b504fd28bb2ffad7b4999c82378d9f5d308fec098657a845329fea080aff40a120a04fa8764eb99cdf7e04069e4120b'
